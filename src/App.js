07b750c38999cdaf53fef700338623fd9ad18bf4
YuliyaMinsk <yuliya.rostova@gmail.com>
2023-01-16 00:01:56 +0300

    feat: add some component and update tests

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div />
+    <div className='main-container'>
+      <h1 className='header'>React Testing library</h1>
+      <button className='button red'>Change to blue</button>
+    </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
+// screen.debug();
 test('button has correct initial color (red)', () => {
   render(<App />);
-  const colorButton = screen.getAllByRole('button', { name: 'Change to blue' });
-  expect(colorButton).toHaveStyle({'background-color': 'red'});
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toHaveClass('red');
 });
 test('button turns blue when clicked, and return to red when clicked again', () => {
   render(<App />);
-  const colorButton = screen.getAllByRole('button', { name: 'Change to blue' });
-  colorButton.simulate('click');
-  expect(colorButton).toHaveStyle({'background-color': 'blue'});
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveClass('blue');
   expect(colorButton).toHaveTextContent('Change to red');
-});
-test('background has correct initial color (green)', () => {
-  render(<App />);
-  const backgroundApp = screen.querySelector('.main-container');
-  expect(backgroundApp).toHaveStyle({'background-color':'green'});
 });
 test('app has text header "React Testing library"', () => {
   render(<App />);
-  const header = screen.getByText('React Testing library');
-  expect(header).toHaveStyle('h1');
+  const header = screen.getByText(/React Testing library/i);
+  expect(header).toBeInTheDocument();
 });
 
